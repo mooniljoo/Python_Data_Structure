ec530cd47d4edb339cb924d50fadbb48c9f1d3a4
iljoomoon <iljoomoon@gmail.com>
2021-04-02 14:05:25 +0900

    Update Maximum Heap.py

---
+++ Heap/MaxHeap.py
@@ -0,0 +1,22 @@
+# 최대 힙
+import heapq
+
+
+def heapsort(iterable):
+    """
+    오름차 순 힙 정렬(Heap Sort)
+    """
+    h = []
+    result = []
+    # 모든 원소를 차례대로 힙에 삽입
+    for value in iterable:
+        heapq.heappush(h, -value)
+    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+    for i in range(len(h)):
+        result.append(-heapq.heappop(h))
+    return result
+
+
+result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(result)
+# [9,8,7,6,5,4,3,2,1,0]
