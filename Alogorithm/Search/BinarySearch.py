bfa10544f68fb47548f9b1ec63faedcd0f861a9a
iljoomoon <iljoomoon@gmail.com>
2021-06-16 17:43:07 +0900

    Create

---
+++ Alogorithm/Search/BinarySearch.py
@@ -0,0 +1,20 @@
+# O(logn)
+def binary_search(data: list, search: int) -> bool:
+    '''
+    Returns boolean whether a value exists in the list
+    '''
+    if len(data) == 1 and search == data[0]:
+        return True
+    if len(data) == 1 and search != data[0]:
+        return False
+    if len(data) == 0:
+        return False
+
+    medium = len(data) // 2
+    if search == data[medium]:
+        return True
+    else:
+        if search > data[medium]:
+            return binary_search(data[medium:], search)
+        else:
+            return binary_search(data[:medium], search)
