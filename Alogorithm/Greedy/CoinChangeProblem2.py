025bf1fd91edb0e6906f3785779de9b8a9377231
iljoomoon <iljoomoon@gmail.com>
2021-06-17 17:31:40 +0900

    Create

---
+++ Alogorithm/Greedy/CoinChangeProblem2.py
@@ -0,0 +1,17 @@
+# O(n)
+coin_list = [500, 100, 50, 1]
+
+
+def min_coin_count(value=int, coin_list=list):
+    total_coin_count = 0
+    details = list()
+    coin_list.sort(reverse=True)
+    for coin in coin_list:
+        coin_num = value // coin
+        total_coin_count += coin_num
+        value -= coin_num * coin
+        details.append([coin, coin_num])
+    return total_coin_count, details
+
+
+print(min_coin_count(4720, coin_list))
