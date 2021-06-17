c993ac13b2c1b952495fcfcc12c50a85769d13bd
iljoomoon <iljoomoon@gmail.com>
2021-06-17 17:31:07 +0900

    Change Name

--- Alogorithm/Greedy/CoinChangeProblem.py
+++ Alogorithm/Greedy/CoinChangeProblem.py
@@ -1,3 +1,4 @@
+# O(n)
 n = int(input("거슬러 주어야 할 금액 입력 : "))
 cnt = 0
 
@@ -5,7 +6,7 @@ cnt = 0
 arr = [500, 100, 50, 10]
 
 for coin in arr:
-    cnt += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전의 갯수 세기
+    cnt += n // coin  # 해당 화폐로 거슬러 줄 수 있는 동전의 갯수 세기
     n %= coin
 
 print(f"거슬러 주어야 하는 동전의 최소 갯수 : {cnt}개")
