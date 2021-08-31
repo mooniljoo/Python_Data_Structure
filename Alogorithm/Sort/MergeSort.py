034c3d7b9d236bd80fcd2e50277dd647305f843d
iljoomoon <iljoomoon@gmail.com>
2021-08-31 16:07:55 +0900

    :construction: 구현 중

--- Alogorithm/Sort/MergeSort.py
+++ Alogorithm/Sort/MergeSort.py
@@ -7,24 +7,29 @@ def split(data=list):
     right = data[medium:]
     print(left, right)
 
+
 def mergesplit(data=list):
-    if len(data) <=1:
+    if len(data) <= 1:
         return data
     medium = int(len(data)/2)
     left = data[:medium]
     right = data[medium:]
     return merge(left, right)
 
+
 def merge(left=list, right=list):
     merge = list()
-    left_point, right_point = 0,0
+    left_point, right_point = 0, 0
 
     # case1 : left/right 아직 남아있을 때
     while len(left) > left_point and len(right) > right_point:
-        if left[left_point]
+        if left[left_point]:
+            pass
 
     # case2 : left만 남아있을 때
     while len(left) > left_point:
+        pass
 
     # case3 : right만 남아있을 때
-    while len(right) > right_point
+    while len(right) > right_point:
+        pass
